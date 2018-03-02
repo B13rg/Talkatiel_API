90dc3b66c5e371488f0ceca6495ce9224e8456a4
B13rg <b11484@gmail.com>
2018-03-01 17:09:07 -0800

    Commit of python-flask based API

---
+++ Reciever.py
@@ -0,0 +1,59 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask.ext.jsonpify import jsonify
+
+db = create_engine('sqlite:///chinook.db')
+app = Flask(__name__)
+api = API(app)
+
+class Posts(Resource):
+	"""Get the top 50 posts.  If a number is passed in, it will get 50 posts from that index onward."""
+	def get(self, post_index=None):
+		conn = db.connect()
+		if post_num is None:
+			#get top x posts
+			query = conn.execute("/*SQL statement LIMIT 50*/")
+		else:
+			#get posts starting from index
+			query = conn.execute("/* SQL statment */")
+		result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+		return jsonify(result)
+	
+	""" Add a post to the database """
+	def post(self):
+		conn = db.connect()
+		print(request.json)
+		#column_name = request.json['column_name']
+		query = conn.execute("INSERT INTO table_name values(null,'{0}')".format(column_name))
+		return {'status':'success'}
+
+class Users(Resource):
+	def get(self, user_id):
+		conn = db.connect()
+		query = conn.execute("SELECT * FROM users WHERE user_id=%d " %float(employee_id))
+		result = {'data': [dict(zip(tuple (query.keys()) , i)) for i in query.cursor]}
+		return jsonify(result)
+
+	def post(self):
+		conn = db.connect()
+		print(request.json)
+		#same as post above.
+
+
+		query = conn.execute("INSERT INTO table_name values()".format())
+		return {'status':'success'}
+
+
+
+
+
+api.add_resource(Posts, '/Posts')	#route 1, probably most used
+api.add_resource(Posts, '/Posts/<post_index>')
+api.add_resource(Users, '/Users')
+api.add_resource(Users, '/Users/<user_id>')
+
+
+if __name__ == '__main__':
+	app.run(port='5002')
